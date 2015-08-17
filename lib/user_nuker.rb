2f7663d4bd3c5146fc5823a23bda2a2bde77eeab
Brandon Weaver <keystonelemur@gmail.com>
2015-08-16 17:49:58 -0700

    Create user_nuker.rb
    Simple way to catch trolls:
    
    ```ruby
    [33] pry(main)> un = UserNuker.new('sreggingnikcuf')
    => #<UserNuker:0x007ff124098630
     @leet_sub={"1"=>"i", "2"=>"z", "3"=>"e", "4"=>"a", "5"=>"s", "6"=>"g", "7"=>"t", "0"=>"o", "@"=>"a", "$"=>"s"},
     @name="sreggingnikcuf",
     @reverse_name="fuckingniggers",
     @wordlist=nil>
    [34] pry(main)> un.is_bad?
    => 3
    
    # http://logs.ryanbigg.com/ruby?date=2015-08-16
    names = log.map { |line| /^\[.+\] (?<name>\S+)\: .+\Z/.match(line)[:name] rescue nil }.compact.uniq
    names.map { |name| [name, UserNuker.new(name).is_bad?] }.select { |(_,s)| s > 0 }.to_h
    => {"n008f4g_"=>1, "NightyMiggers"=>1, "sreggingnikcuf"=>3}
    ```

---
+++ lib/user_nuker.rb
@@ -0,0 +1,92 @@
+class UserNuker
+  attr_reader :name
+
+  def initialize(name, wordlist = nil)
+    @name         = clean(name)
+    @reverse_name = @name.reverse
+    @wordlist     = wordlist
+  end
+
+  # Checks to see if the username is in any way related to a naughty
+  # word, including its reverse.
+  #
+  # @return [Integer] Count of hits
+  def is_bad?
+    combinations.select { |c|
+      c.include?(@name)         ||
+      c.include?(@reverse_name) ||
+      @name.include?(c)         ||
+      @reverse_name.include?(c)
+    }.count
+  end
+
+  # Our naughty wordlist
+  #
+  # @return [Array[String]]
+  def wordlist
+    @wordlist ||= %w(
+      fuck
+      migger
+      nigger
+      bitch
+      cunt
+      fag
+      faggot
+      damn
+      damnit
+      shit
+      shitt
+    )
+  end
+
+  # Potential word endings to look for as well to get
+  # a stronger match
+  #
+  # @return [Array[String]]
+  def endings
+    @endings ||= %w(
+      ing
+      er
+      it
+    )
+  end
+
+  # Combinations of words with endings
+  #
+  # @return [Array[String]]
+  def combinations
+    @combinations ||=
+      wordlist + wordlist.product(endings).flat_map(&:join)
+  end
+
+  # Take care of simple character substitution
+  #
+  # @return [Hash[String, String]]
+  def leet_sub
+    @leet_sub ||= {
+      ?1 => ?i,
+      ?2 => ?z,
+      ?3 => ?e,
+      ?4 => ?a,
+      ?5 => ?s,
+      ?6 => ?g,
+      ?7 => ?t,
+      ?8 => ?b,
+      ?0 => ?o,
+      ?@ => ?a,
+      ?$ => ?s
+    }
+  end
+
+  # Cleans a name of spaces and other unnecessary characters,
+  # and translates to alphabetical characters
+  # 
+  # @param name [String] Original name
+  #
+  # @return [String] cleaned variant of the name
+  def clean(name)
+    name.downcase.delete('_ ').chars.map { |c|
+      leet_sub[c] || c
+    }.join
+  end
+end
